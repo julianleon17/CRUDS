9556e10812d4e2f69a20986acf00b96f12d65740
Julian Leon <juliancho3452@gmail.com>
2021-03-16 16:26:19 -0500

    El crud multi-data ahora puede borrar historial y tiene requerimiento

--- Data/multi-data/read/read.php
+++ Data/multi-data/read/read.php
@@ -1,51 +1,13 @@
 <?php
+
 $filename = "../baseDatos.db";
-$pedidos = file_get_contents( $filename );
+
+$mensaje;
+
+if ( file_exists( $filename ) ) {
+    $mensaje = " (El historial ya existe)";
+}else{
+    $mensaje = " (El historial aún no existe)";
+}
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Historial</title>
-
-    <link rel="stylesheet" href="../../../CSS/styles.css">
-    
-<style>
-        
-*{
-    text-align: left;
-}
-
-h1{
-    text-align: center;
-}   
-
-.delete a{
-    color: white;
-    background-color: red;
-}
-
-
-</style>
-
-</head>
-<body>
-    <h1>Historial de Pedidos</h1>
-    <?php echo $pedidos; ?>
-
-    <div class="menu" >
-        <ul>
-            <div class="opcion" >
-                <li> <a href="../index.html"> Volver al Inicio </a> </li>
-            </div>
-
-            <div class="opcion" >
-                <li class="delete" > <a  href="delete-multidata.php"> Eliminar Historial </a> </li>
-            </div>
-        </ul>
-    </div>
-</body>
-</html>
